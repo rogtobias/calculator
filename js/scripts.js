8b6b4533c36eedc34bfc5ea3a7ae1262e7611089
david rolfs and Roger Tobias <roger@>
2017-04-24 10:35:10 -0700

    change result name

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,5 @@ var divide = function(number1, number2) {
 var number1 = parseInt(prompt("Give me a number: "));
 var number2 = parseInt(prompt("Give me another number: "));
 
-var sum = add(number1, number2);
-alert(sum);
+var result = divide(number1, number2);
+alert(result);
