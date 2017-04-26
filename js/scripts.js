34261556b238a31e8c122f54971f3ce674f0b071
Pete Lazuran and Roger Tobias <pete@>
2017-04-26 11:50:40 -0700

    JS writen, working to debug.

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,31 @@ var divide = function(number1, number2) {
 //front end logic
 $(document).ready(function() {
 	$("form#add").submit(function(event) {
-		event.preventDefault();
 		var number1 = parseInt($("#add1").val());
 		var number2 = parseInt($("#add2").val());
 		var result = add(number1, number2);
 		$("#output").text(result);
+		event.preventDefault();
+	});
+	$("form#sub").submit(function(event) {
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		var result = subtraction(number1, number2);
+		$("#outputS").text(result);
+		event.preventDefault();
+	});
+	$("form#mult").submit(function(event) {
+		var number1 = parseInt($("#mult1").val());
+		var number2 = parseInt($("#mult2").val());
+		var result = multiply(number1, number2);
+		$("#outputM").text(result);
+		event.preventDefault();
+	});
+	$("form#division").submit(function(event) {
+		var number1 = parseInt($("#division1").val());
+		var number2 = parseInt($("#division2").val());
+		var result = divide(number1, number2);
+		$("#outputD").text(result);
+		event.preventDefault();
 	});
 });
